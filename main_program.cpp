42ac7fed878ff9fe269ba618c5d5b18f05fe97f9
mirfan11 <47858424+mirfan11@users.noreply.github.com>
2019-04-15 03:05:13 +0700

    deklarasi header ,struct, dan fungsi

---
+++ main_program.cpp
@@ -0,0 +1,19 @@
+//Nama 	: Muhammad Irfan 
+//NPM 	:1817051071
+//Subjek :program Stack dengan konsep linked list 
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct node{
+	int value ;
+	node *next ;
+	
+}*top =NULL;
+
+bool isEmpty();
+void push (int Newdata);
+void pop ();
+void viewStack();
+char lagi (char pilih);
