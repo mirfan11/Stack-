f92a69c566c1793aae6848326a5fa6307d5b83c2
mirfan11 <47858424+mirfan11@users.noreply.github.com>
2019-04-15 03:10:43 +0700

    Main program

--- main_program.cpp
+++ main_program.cpp
@@ -16,7 +16,53 @@ bool isEmpty();
 void push (int Newdata);
 void pop ();
 void viewStack();
-char lagi (char pilih);
+
+int main(){
+	int menu, nilai;
+	char pilih;
+	while (menu!=4){
+		cout<<"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		cout<<"Menu Program Stack : \n";
+		cout<<"1. Push\n";
+		cout<<"2. Pop\n";
+		cout<<"3. Tampilkan\n";
+		cout<<"4. Exit\n";
+		cout<<"Pilih: ";
+		cin>>menu;
+		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		switch(menu)
+		{
+			case 1: 
+				do{
+					cout<<"Masukkan Nilai : ";
+					cin>>nilai;
+					push(nilai);
+					cout<<"\ningin memasukkan nilai lagi (y/Y) : ";
+					cin>>pilih;
+				}while (pilih=='y'||pilih =='Y');
+				break ;
+			
+			case 2:
+				do{
+					pop();
+					cout<<"ingin di Pop lagi (y/Y) : ";
+					cin>>pilih;
+				}while (pilih=='y'||pilih =='Y');
+				break ;
+				
+			case 3 :
+				cout<<"Isi Stack :\n";
+				viewStack();
+				break;
+			case 4:
+				cout<<"\nTelah keluar dari Program. Terimakasih\n";
+				break;
+			default :
+				cout<<"\nMenu pada program tidak tersedia , silahkan ulangi kembali \n";
+				break;
+		}
+	}
+}
 
 bool isEmpty (){
 	return (top==NULL);
